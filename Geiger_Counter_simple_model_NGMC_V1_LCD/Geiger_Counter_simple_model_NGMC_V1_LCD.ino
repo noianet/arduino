4d5c8757405442dae9d3db3f45b916a60c576180
noianet <stale.ness@gmail.com>
2017-04-27 00:06:57 +0200

    Battery voltage read long press.

--- Geiger_Counter_simple_model_NGMC_V1_LCD/Geiger_Counter_simple_model_NGMC_V1_LCD.ino
+++ Geiger_Counter_simple_model_NGMC_V1_LCD/Geiger_Counter_simple_model_NGMC_V1_LCD.ino
@@ -1,29 +1,38 @@
 /*
-GPL ref, based on https://github.com/majek/dump/blob/master/arduino/ard-01/geiger.pde
+    GPL ref, based on https://github.com/majek/dump/blob/master/arduino/ard-01/geiger.pde
 
-Geiger counter details:
-  Geiger interrupt 0 (pin D2 on arduino mini - see attachInterrupt).
-  Onboard led swap on/off on geiger read (13 on mini)
-  Serial USB output 115200. Indicates CPM, µSv og µSv average (floating average over 10 readings)
+    HW: Should add 10uF cap between gnd and reset to avoid reset when enabling serial.
+
+    Geiger counter details:
+    Geiger interrupt 0 (pin D2 on arduino mini - see attachInterrupt).
+    Onboard led swap on/off on geiger read (13 on mini)
+    Serial USB output 115200. Indicates CPM, µSv og µSv average (floating average over 10 readings)
      Example output: cpm: 20 uSv/h: 0.16 uSv/h avg: 0.17
-  Softserial for bluetooth mobile reporting. Adapted for app  "Bluetooth Terminal/Graphics" by Emrecan ÇETİN. Uses pin 11=RX, 12=TX (RX not in use for now)
-     Format required by app:  Evalue1,value2,value3...\n. Data sent are: cpm,usv,usv_accumulated
+    Softserial for bluetooth mobile reporting. Adapted for app  "Bluetooth Terminal/Graphics" by Emrecan ÇETİN. Uses pin 11=RX, 12=TX (RX not in use for now)
+     Format required by app:  Evalue1,value2,value3...\n. Data sent are: cpm,usv_average,usv_accumulated
      Using HC-06 bt device, usually pairing passcode is 1234.
-  LCD support I2C, A4=SDA, A5=SCL.
+    LCD support I2C, A4=SDA, A5=SCL.
      Pin A0 to switch LCD backlight on/off.  (Internal pullup) TODO - auto timeout?
-  
-  External 5 LED row indicator support with fade for increased resolution. Indicates counts pr. 10/sec.
 
-  TODO: Store to SD card?
-  TODO: Buttons to do some stuff?
-  TODO: rad accumulator calculation and a "run away" warning?
+    External 5 LED row indicator support with fade for increased resolution. Indicates counts pr. 10/sec.
+
+
+    TODO: Piezo off when screen is off. 3 modes? Disable onboard LED. HWmod.
+    TODO: Hold mode button x sec to display VCC voltage.  Added readVcc() function.
+    TODO:
+
+    Misc bloat ideas:.
+    TODO: Store to SD card?
+    TODO: Buttons to do some stuff?
+    TODO: rad accumulator calculation and a "run away" warning?
 */
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 // initialize the library with the numbers of the interface pins
 LiquidCrystal_I2C lcd(0x3F, 20, 4);
-SoftwareSerial btSerial(11, 12); // RX, TX for bluetooth adapter 
+SoftwareSerial btSerial(11, 12); // RX, TX for bluetooth adapter
 
 #define LOG_PERIOD 15000  //Logging period in milliseconds, recommended value 15000-60000. 
 #define MAX_PERIOD 60000  //Maximum logging period without modifying this sketch
@@ -35,11 +44,18 @@ SoftwareSerial btSerial(11, 12); // RX, TX for bluetooth adapter
 //#define USV_CONVERSION 175.43 //for SBM-20 tube
 
 // CPS threshold values for the led bar (resets 10 times pr sec),
-#define TH0 40
+/*#define TH0 40
 #define TH1 80
 #define TH2 120
 #define TH3 160
 #define TH4 200
+*/
+//DEBUG VALUES - only have low radiation source.
+#define TH0 20
+#define TH1 40
+#define TH2 80
+#define TH3 120
+#define TH4 160
 
 //Pins used for LED array. Must be PWM capable
 int ledArray [] = {3, 5, 6, 9, 10};
@@ -52,179 +68,226 @@ unsigned int multiplier;  //variable for calculation CPM in this sketch
 unsigned long previousMillis;  //variable for time measurement
 unsigned long previousLedMillis;  //variable for time measurement LED barreset
 unsigned long debounceMillis;   // debounce time variable for input buttons
+int  ButtonStateCounter = 0;// hold counter.
 float usv_average = 0.20; //variable for uSv, starting with avg. 0.20
 float usv_average_old = 0.20; //variable for uSv last reading for LCD arrow, starting with avg. 0.20
 float usv_accumulated = 0; //variable.for accumulated since boot/reset
 boolean lcd_mode = 1; //used to swap LCD info
-boolean lcdbacklightstate=1; //backlight state (on at boot)
+boolean lcdbacklightstate = 1; //backlight state (on at boot)
 
 
 void setup() {
-  multiplier = MAX_PERIOD / LOG_PERIOD;      //calculating multiplier, depend on your log period
-  Serial.begin(115200);
-  btSerial.begin(9600);
-  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
-  attachInterrupt(0, tube_impulse, FALLING);   //define external interrupt 0
+    multiplier = MAX_PERIOD / LOG_PERIOD;      //calculating multiplier, depend on your log period
+    Serial.begin(115200);
+    btSerial.begin(9600);
+    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    attachInterrupt(0, tube_impulse, FALLING);   //define external interrupt 0
 
-  ledFade(TH4 - 1); //all on LEDBAR
-  
-  lcd.init();                      // initialize the lcd
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Radiation Sensor");
-  lcd.setCursor(2, 1);
-  lcd.print("Please wait");
+    ledFade(TH4 - 1); //all on LEDBAR
 
-  pinMode(BACKLIGHTBUTTON, INPUT_PULLUP);  //input push button for backlight switching
-  
-  //flash onboard led to indicate setup done
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  delay(1500);
-  ledFade(TH2 - 1); //ledbar medium
-  digitalWrite(13, LOW);
-  delay(1000);
+    lcd.init();                      // initialize the lcd
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Radiation Sensor");
+    lcd.setCursor(2, 1);
+    lcd.print("Please wait");
 
-  ledFade(0); //zero ledbar
+    pinMode(BACKLIGHTBUTTON, INPUT_PULLUP);  //input push button for backlight switching
 
-  Serial.println("Arduino setup complete");
+    //flash onboard led to indicate setup done
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+    delay(1500);
+    ledFade(TH2 - 1); //ledbar medium
+    digitalWrite(13, LOW);
+    delay(1000);
+
+    ledFade(0); //zero ledbar
+
+    Serial.println("Arduino setup complete");
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis();
 
-  //LCD and serial output/updates millis check
-  if (currentMillis - previousMillis > LOG_PERIOD) {
-    previousMillis = currentMillis;
-    cpm = counts * multiplier; //adjustable by constants
+    //LCD and serial output/updates millis check
+    if (currentMillis - previousMillis > LOG_PERIOD) {
+        previousMillis = currentMillis;
+        cpm = counts * multiplier; //adjustable by constants
 
-    //const float conversion_factor
-    float usv = (float)cpm / USV_CONVERSION;
-    usv_average = ((usv_average * 9 + usv) / 10);
-    usv_accumulated = usv_accumulated + (usv / (MAX_PERIOD/LOG_PERIOD)) / 60; //accumulated since boot/reset
+        //const float conversion_factor
+        float usv = (float)cpm / USV_CONVERSION;
+        usv_average = ((usv_average * 9 + usv) / 10);
+        usv_accumulated = usv_accumulated + (usv / (MAX_PERIOD / LOG_PERIOD)) / 60; //accumulated since boot/reset
 
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    if (lcd_mode) { //swaps between two LCD infos on first line
-      if (cpm < 10) {
-        lcd.print(" "); //add spaces to avoid jumping on 1, 10 digit numbers
-      }
-      if (cpm < 100) {
-        lcd.print(" ");
-      }
-      if (cpm < 1000) {
-        lcd.print(" ");
-      }
-      lcd.print(cpm);
-      lcd.print("cpm");
-      lcd.setCursor(8, 0);
-      lcd.print(usv, 2);
-      lcd.print((char)228); //special char µ
-      lcd.print("Sv");
-      lcd_mode = 0;
-    } else {
-      lcd.print(" ");
-      lcd.print((char)228); //special char µ
-      lcd.print("Sv acc: ");
-      lcd.print(usv_accumulated);
-      lcd_mode = 1;
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        if (lcd_mode) { //swaps between two LCD infos on first line
+            if (cpm < 10) {
+                lcd.print(" "); //add spaces to avoid jumping on 1, 10 digit numbers
+            }
+            if (cpm < 100) {
+                lcd.print(" ");
+            }
+            if (cpm < 1000) {
+                lcd.print(" ");
+            }
+            lcd.print(cpm);
+            lcd.print("cpm");
+            lcd.setCursor(8, 0);
+            lcd.print(usv, 2);
+            lcd.print((char)228); //special char µ
+            lcd.print("Sv");
+            lcd_mode = 0;
+        } else {
+            lcd.print(" ");
+            lcd.print((char)228); //special char µ
+            lcd.print("Sv acc: ");
+            lcd.print(usv_accumulated);
+            lcd_mode = 1;
+        }
+        lcd.setCursor(0, 1); //second line
+        if (usv_average > usv_average_old) {
+            lcd.print((char)126); //special char, right arrow (rom missing up)
+        } else if (usv_average < usv_average_old) {
+            lcd.print((char)127); //special char, left arrow (rom missing down)
+        }  else {
+            lcd.print(" "); //no change, space only
+        }
+        lcd.print(usv_average, 3);
+        lcd.print((char)228); //special char µ
+        lcd.print("Sv/h avg");
+        usv_average_old = usv_average; //update with last reading
+
+        //USB serial
+        Serial.print("cpm: ");
+        if (cpm < 10) {
+            Serial.print(" "); //adds extra space if single digit to clean up formatting
+        }
+        //if (cpm < 100) {Serial.print(" ");} //adds extra space if single digit to clean up formatting
+        //if (cpm < 1000) {Serial.print(" ");} //adds extra space if single digit to clean up formatting
+        Serial.print(cpm);
+        Serial.print(" uSv/h: ");
+        Serial.print(usv);
+        Serial.print(" uSv/h avg: ");
+        Serial.print(usv_average);
+        Serial.print(" uSv/h acc: ");
+        Serial.println(usv_accumulated, 4);
+
+        //Bluetooth softserial
+        btSerial.print("E");
+        btSerial.print(cpm);
+        btSerial.print(",");
+        btSerial.print(usv_average);
+        btSerial.print(",");
+        btSerial.print(usv_accumulated);
+        btSerial.print("\n");
+
+        counts = 0;
     }
-    lcd.setCursor(0, 1); //second line
-    if (usv_average > usv_average_old) {
-      lcd.print((char)126); //special char, right arrow (rom missing up)
-    } else if (usv_average < usv_average_old) {
-      lcd.print((char)127); //special char, left arrow (rom missing down)
-    }  else {
-      lcd.print(" "); //no change, space only
-    }
-    lcd.print(usv_average, 3);
-    lcd.print((char)228); //special char µ
-    lcd.print("Sv/h avg");
-    usv_average_old = usv_average; //update with last reading
 
-    //USB serial
-    Serial.print("cpm: ");
-    if (cpm < 10) {
-      Serial.print(" "); //adds extra space if single digit to clean up formatting
+    //Ledbar millis check
+    if (currentMillis - previousLedMillis > 100) { //run 10 times pr second
+        previousLedMillis = currentMillis;
+        ledFade(ledcps);
+        ledcps = 0; //reset counter
     }
-    //if (cpm < 100) {Serial.print(" ");} //adds extra space if single digit to clean up formatting
-    //if (cpm < 1000) {Serial.print(" ");} //adds extra space if single digit to clean up formatting
-    Serial.print(cpm);
-    Serial.print(" uSv/h: ");
-    Serial.print(usv);
-    Serial.print(" uSv/h avg: ");
-    Serial.print(usv_average);
-    Serial.print(" uSv/h acc: ");
-    Serial.println(usv_accumulated, 4);
 
-    //Bluetooth softserial
-    btSerial.print("E");
-    btSerial.print(cpm);
-    btSerial.print(",");
-    btSerial.print(usv);
-    btSerial.print(",");
-    btSerial.print(usv_accumulated);
-    btSerial.print("\n");
+    //LCD button check
+    if (currentMillis - debounceMillis > 500 || (!digitalRead(BACKLIGHTBUTTON) && !ButtonStateCounter)) {  //check interval. Or here to catch early press
+        debounceMillis = currentMillis; // reset counter
+        if (!digitalRead(BACKLIGHTBUTTON)) ButtonStateCounter++; // Button is held down, increase button counter
 
-    counts = 0;
-  } 
-  
-  //Ledbar millis check
-  if (currentMillis - previousLedMillis > 100) { //run 10 times pr second
-    previousLedMillis = currentMillis;
-    ledFade(ledcps);
-    ledcps = 0; //reset counter
-  }
-  
-  //LCD backlight button millis check (to avod button bounce issues)
-  if (currentMillis - debounceMillis > 500 && !digitalRead(BACKLIGHTBUTTON)) { 
-    debounceMillis = currentMillis;
-    if (lcdbacklightstate) {
-      lcd.noBacklight(); // turn off backlight
-      lcdbacklightstate=0;
-    } else {
-      lcd.backlight(); // turn on backlight.
-      lcdbacklightstate=1;
+        //button release events. Checks counter to do stuff
+        if (digitalRead(BACKLIGHTBUTTON) && ButtonStateCounter == 1) { //Button state counter larger than 0, and button is released.
+            if (lcdbacklightstate) {
+                lcd.noBacklight(); // turn off backlight
+                lcdbacklightstate = 0; debounceMillis;
+            } else {
+                lcd.backlight(); // turn on backlight.
+                lcdbacklightstate = 1;
+            }
+        }
+
+        if (ButtonStateCounter == 2)  {  //long press 1. Just do show battery level. Do not reset counter here, done below when button released.
+            float vccread = readVcc();
+            vccread = vccread / 1000; //convert from mv to V
+            lcd.backlight(); // turn on backlight in case it is off
+            lcdbacklightstate = 1;
+            lcd.clear();
+            lcd.setCursor(0, 0); //first line
+            lcd.print("Battery Voltage:");
+            lcd.setCursor(2, 1); //second line
+            lcd.print(vccread);
+            lcd.print("V ");
+        }
+        //Might add more stuff here for event 4, 6, 8 etc to count through every 2. sec..
+        if (digitalRead(BACKLIGHTBUTTON)) ButtonStateCounter = 0; // Cleanup and reset button counter. Events handled above.
     }
-  }
+} //end loop
+//-----------------------------Functions/events below here----------------------------
+void tube_impulse() {  //function for capturing interrupt events from Geiger Kit
+    counts++;
+    ledcps++;
+    //switch state onboard led to indicate response
+    digitalWrite(13, !digitalRead(13));
 }
 
 void ledFade(int value) { //function for 5x LED row update with fading effect for increased resolution.
-  //DEBUG: Print current value
-  //Serial.print(" LedCPdS: : ");
-  //Serial.println(map(value,0,TH0,0,255));
-  if (value < TH0) {
-    analogWrite(ledArray[0], map(value, 0, TH0, 0, 255));
-    digitalWrite(ledArray[1], LOW); digitalWrite(ledArray[2], LOW); digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
-  } else if (value < TH1) {
-    digitalWrite(ledArray[0], HIGH);
-    value =- TH0; //delete previous threshold
-    analogWrite(ledArray[1], map(value, 0, TH1, 0, 255));
-    digitalWrite(ledArray[2], LOW); digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
-  } else if (value < TH2) {
-    digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH);
-    value =- TH1;//delete previous threshold
-    analogWrite(ledArray[2], map(value, 0, TH2, 0, 255));
-    digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
-  } else if (value < TH3) {
-    digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH);
-    value =- TH2;//delete previous threshold
-    analogWrite(ledArray[3], map(value, 0, TH3, 0, 255));
-    digitalWrite(ledArray[4], LOW);
-  } else if (value < TH4) {
-    digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH); digitalWrite(ledArray[3], HIGH);
-    value =- TH3;//delete previous threshold
-    analogWrite(ledArray[4], map(value, 0, TH4, 0, 255));
-  } else {
-    digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH); digitalWrite(ledArray[3], HIGH); digitalWrite(ledArray[4], HIGH);
-  }
+    //DEBUG: Print current value
+    //Serial.print(" LedCPdS: : ");
+    //Serial.println(map(value,0,TH0,0,255));
+    if (value < TH0) {
+        analogWrite(ledArray[0], map(value, 0, TH0, 0, 255));
+        digitalWrite(ledArray[1], LOW); digitalWrite(ledArray[2], LOW); digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
+    } else if (value < TH1) {
+        digitalWrite(ledArray[0], HIGH);
+        value = - TH0; //delete previous threshold
+        analogWrite(ledArray[1], map(value, 0, TH1, 0, 255));
+        digitalWrite(ledArray[2], LOW); digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
+    } else if (value < TH2) {
+        digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH);
+        value = - TH1; //delete previous threshold
+        analogWrite(ledArray[2], map(value, 0, TH2, 0, 255));
+        digitalWrite(ledArray[3], LOW); digitalWrite(ledArray[4], LOW);
+    } else if (value < TH3) {
+        digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH);
+        value = - TH2; //delete previous threshold
+        analogWrite(ledArray[3], map(value, 0, TH3, 0, 255));
+        digitalWrite(ledArray[4], LOW);
+    } else if (value < TH4) {
+        digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH); digitalWrite(ledArray[3], HIGH);
+        value = - TH3; //delete previous threshold
+        analogWrite(ledArray[4], map(value, 0, TH4, 0, 255));
+    } else {
+        digitalWrite(ledArray[0], HIGH); digitalWrite(ledArray[1], HIGH); digitalWrite(ledArray[2], HIGH); digitalWrite(ledArray[3], HIGH); digitalWrite(ledArray[4], HIGH);
+    }
 }
 
-void tube_impulse() {  //function for capturing interrupt events from Geiger Kit
-  counts++;
-  ledcps++;
-  //switch state onboard led to indicate response
-  digitalWrite(13, !digitalRead(13));
+long readVcc() {
+    // Read 1.1V reference against AVcc
+    // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+    delay(2); // Wait for Vref to settle
+    ADCSRA |= _BV(ADSC); // Start conversion
+    while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+    uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+    uint8_t high = ADCH; // unlocks both
+
+    long result = (high << 8) | low;
+
+    result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+    return result; // Vcc in millivolts
 }
 
 

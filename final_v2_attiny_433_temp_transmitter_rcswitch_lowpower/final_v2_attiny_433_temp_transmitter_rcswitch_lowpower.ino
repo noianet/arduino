287fdab033b12d07e5db373ec3f8bfad78949112
noianet <stale.ness@gmail.com>
2017-04-27 00:12:33 +0200

    Misc vref fixes etc. + serial debug.

--- final_v2_attiny_433_temp_transmitter_rcswitch_lowpower/final_v2_attiny_433_temp_transmitter_rcswitch_lowpower.ino
+++ final_v2_attiny_433_temp_transmitter_rcswitch_lowpower/final_v2_attiny_433_temp_transmitter_rcswitch_lowpower.ino
@@ -1,153 +1,194 @@
-/* 
-reminder:
-- Added averaging, can remove at receiver script
-- Added vcc calculation
-- added external vref. Tie pin 5 high with 10k resistor
-- disabled fudge hack...
-- removed wdt flag/if
-*/ 
-#include <RCSwitch.h>
+/*
+    Using TMP36 sensor with internal ATTINY. External 3.3v  regulator for stability (internal 1v1 was not stable).
+    Temperature calues are times 100 to get 2 decimal, reduce on receiver.
+    Prefix'es for radio transmission in receiver script:
+    90xxxx Normal  C
+    91xxxx Negative C
+    92xxxx VCCvoltage, result is milivolt
+    97xxxx Debug flag - not used for now
+    98xxxx Debug warning flag - not used for now
+    99 Boot indicator.
+
+*/
+#define DEBUG 0 //if set to debug, uses serial out instead of radio. NB: TX uses same pin as radio so switch in HW too.
+
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <math.h>
 
-//watchdog flag
-int counter = 100; //high to force immediate read on boot.
-float oldtemp = 22; //guess something... will influence first read at boot.
 
-//correct to batt.
-//float aref_voltage = 5.10;         
-//float aref_voltage = 4.94;
+#define TEMPSENSORPIN A2 //physpin 3, A2, PB4
+#define SUPPLYVOLTAGE 1.1 //1.1V for internal reference. Adjust accordingly. Works best with 3.3v supply for ATTINY and TMP36
+#define SLEEPLOOP 10 //sleep loops before check/transmit. x*8s. Set to 80 in prod
+#define VCCREPORT 10 //report interval for battery voltage. Set to 100 for prod. Counter goes up 1 every tempreport.
 
-RCSwitch mySwitch = RCSwitch();
 
+#ifdef DEBUG
+#include <TinyDebugSerial.h>
+TinyDebugSerial mySerial = TinyDebugSerial();
+#else
+include <RCSwitch.h>
+RCSwitch radio = RCSwitch();
+#endif
+
+
+
+float temperature;
+float oldtemp = 20;
+int tempcounter = SLEEPLOOP; //sleep loop counter, set high to force immediate read on boot.
+int vcccounter = VCCREPORT; //initially set high to trigger battery voltage report on boot.
 
 void setup() {
-   // Transmitter is connected to Attiny Pin PB3  <--
-   // That is physical pin2
-  mySwitch.enableTransmit(3);
-  pinMode(A2, INPUT); //temp read analog. Let it float. PhysPin 3
-  pinMode(1, OUTPUT); //used to power to temp sensor. PhysPin 6
-  digitalWrite(1,LOW); //for consistency
+#ifdef DEBUG
+    mySerial.begin( 9600 ); //using Pin PB3 phys pin2 also for serial, so disable radio transmission if used.
+#else
+    // Transmitter is connected to Attiny Pin PB3, That is physical pin2
+    radio.enableTransmit(3);
+#endif
 
-  //tie unused to ground
- pinMode(5, OUTPUT); //physpin 1
- digitalWrite(5, LOW);
- pinMode(2, OUTPUT); //physpin 7
- digitalWrite(2, LOW);
- pinMode(0, OUTPUT); //physpin 5  
- digitalWrite(0, LOW);
-  
-  setup_watchdog(9); // enable watchdog service
-  
-  //indicate startup
-  delay(1000);
-  mySwitch.send(29, 24); //indicate boot
-  delay(1000); //initial wait, just in case...
+
+    setup_watchdog(9); // enable watchdog service. (9) = 8s (max sleep interval)
+
+    analogReference(INTERNAL1V1); //use internal 1.1V reference for analog read. adjust SUPPLYVOLTAGE if activated.
+
+    //Enable internal pullups on unused pins for noise suppression.
+    pinMode(0, INPUT_PULLUP); //physpin 5. Not analog in/might be overkill
+    pinMode(1, INPUT_PULLUP); //physpin 6. Not analog in/might be overkill
+    pinMode(2, INPUT_PULLUP); //physpin 7
+    //pin 3 used for tempsensor (physpin 2)
+    //pin 4 used for radio or serial TX debugging (physpin 3).
+
+    //indicate startup
+    delay(1000);
+#ifdef DEBUG
+    mySerial.println("ATTINY boot");
+#else
+    radio.send(99, 24); //indicate boot
+    delay(1000);
+#endif
 }
- 
+
 void loop() {
-    if (counter < 4) { //sleep ticks. x*8s. set to 80 in prod
-        //mySwitch.send(27, 24); //DEBUG, send too indicate sleep tick. 
-        counter = counter + 1;
+    if (tempcounter < SLEEPLOOP) {
+        //mySwitch.send(27, 24); //DEBUG, send too indicate sleep tick.
+        tempcounter++;
         sleep();
     } else {
-        //sleep ticks finished, check and report temperature
-        //turn on sensor, read, turn off.
-        digitalWrite(1,HIGH);
-        //delay(100); <--was too short
-        sleep(); //let the thing stabilize for 8 sec.
+        delay(200); //might help stabilize ADC..
+        int pinread  = analogRead(TEMPSENSORPIN); //extra read to avoid ghost voltage
+        pinread  = analogRead(TEMPSENSORPIN);
+        //temperature = analogRead(TEMPSENSORPIN) * AREF * 0.1 - 50.0; // value to celcius conversion for TMP36
+        float voltage = pinread * SUPPLYVOLTAGE / 1024; //convert reading to milivolt
+        float temperature = (voltage - 0.5) * 100 ; //converting from 10 mv per degree.
 
-        long vcc_mv=readVcc(); //calculate VCC, return is milivolt
-        mySwitch.send(220000+vcc_mv, 24); //DEBUG <--------------------------------------------------------
-        
-        float reading=analogRead(A2); 
-        delay(10);    
-        digitalWrite(1,LOW);
-        
-        // converting that reading to voltage, Ref voltage set at top
-        //float voltage = reading * aref_voltage; --old static thing, remove after test
-        float voltage = reading * (vcc_mv/1000);
-        voltage /= 1024.0; //converting reading to true 0-xV voltage
-        //fudge factor...
-        //voltage=voltage+0.11;
-     
-        float temperatureFL = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree with 500 mV offset to degrees ((voltage - 500mV) times 100)
+        //average 2 reads. Times 100 to get 2 decimal points for transmission. Reduced at receiver
+        int tempaverage = (temperature * 100 + oldtemp * 100) / 2;
 
-        int temperature = round(temperatureFL*100); //times 100 to get 2 decimals. reduced on reciever
-      
-        //voltage=voltage*1000;//DEBUG
-        //mySwitch.send(990000+voltage, 24); //DEBUG
-
-        //averaging with previous reading
-        int avrg = round((temperature + oldtemp) / 2);
-        
-        if (avrg == oldtemp) {
+#ifdef DEBUG
+        //Debug. Enable tinySerial for output. NB: Uses radio transmit pin.
+        float vccvoltage = readVcc();
+        vccvoltage = vccvoltage / 1000; //reduce from milivolt
+        mySerial.print("Pinread: ");
+        mySerial.print(pinread);
+        mySerial.print(" V_calc: ");
+        mySerial.print(voltage, 3);
+        mySerial.print(" VCC_calc: ");
+        mySerial.print(vccvoltage, 3);
+        mySerial.print(" T_calc: ");
+        mySerial.print(temperature, 2);
+        mySerial.print(" T_avrg: ");
+        mySerial.println(tempaverage);
+        //End debug part.
+#else
+        //radio transmission part.
+        if (temperature == oldtemp) {
             //temp unchanged, do nothing
-        } else if (avrg >= 0) { //if to handle pos/negative values prefixing
-            mySwitch.send(200000+avrg, 24);
-        } else { //negative temp, change to positive and use 21 prefix
-            avrg=fabsf(avrg);
-            mySwitch.send(210000+avrg, 24);
+        } else if (tempaverage >= 0) { //if to handle pos/negative values prefixing
+            radio.send(900000 + tempaverage, 24);
+        } else { //negative temp, change to positive number and use 21 prefix
+            tempaverage = fabsf(tempaverage);
+            radio.send(910000 + tempaverage, 24);
         }
         oldtemp = temperature;
-        counter = 1; //reset counter
+
+        //report VCC voltage (milivolt) - run only once every VCCREPORT temp reports
+        if (vcccounter < VCCREPORT) {
+            vcccounter++;
+        } else {
+            delay(500); //short wait for next radio transmission to stabilize vcc
+            float vccvoltage = readVcc(); //get VCC voltage, result is milivolt.
+            delay(10); //extra wait before radio mess
+            radio.send(920000 + vccvoltage, 24);
+            vcccounter = 1; //reset counter
+        }
+        //End radio transmission part
+#endif
+
+        //reset counter and go to sleep
+        tempcounter = 1; //reset counter
         sleep();  //go to interrupt sleep
-    } 
+    }
 }
 
-//reference for this bit here: http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
 long readVcc() {
-  // Read 1.1V reference against AVcc
-  // set the reference to Vcc and the measurement to the internal 1.1V reference. Following is ATTINY85 specific (change for different micro).
-  ADMUX = _BV(MUX3) | _BV(MUX2);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Start conversion
-  while (bit_is_set(ADCSRA,ADSC)); // measuring
- 
-  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
-  uint8_t high = ADCH; // unlocks both
- 
-  long result = (high<<8) | low;
- 
-  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
-  return result; // Vcc in millivolts
+    // Read 1.1V reference against AVcc
+    // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+    delay(2); // Wait for Vref to settle
+    ADCSRA |= _BV(ADSC); // Start conversion
+    while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+    uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+    uint8_t high = ADCH; // unlocks both
+
+    long result = (high << 8) | low;
+
+    result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+    return result; // Vcc in millivolts
 }
 
 // Watchdog Interrupt Service / is executed when watchdog timed out
 ISR(WDT_vect) {
-  //nothing here, just go to loop
+    //nothing here, just go to loop
 }
 
-
-// Below routine to set system into the sleep state 
+// Below routine to set system into the sleep state
 // system wakes up when wtchdog is timed out
 void sleep() {
-  ADCSRA &= ~_BV(ADEN);                // ADC off
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
-  sleep_enable();
-  sleep_mode();                        // System sleeps here
-  sleep_disable();                     // System continues execution here when watchdog timed out 
-  ADCSRA |= _BV(ADEN);                 // ADC on
+    ADCSRA &= ~_BV(ADEN);                // ADC off
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+    sleep_enable();
+    sleep_mode();                        // System sleeps here
+    sleep_disable();                     // System continues execution here when watchdog timed out
+    ADCSRA |= _BV(ADEN);                 // ADC on
 }
 
 // 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
 // 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
 void setup_watchdog(int ii) {
-  byte bb;
-  int ww;
-  if (ii > 9 ) ii=9;
-  bb=ii & 7;
-  if (ii > 7) bb|= (1<<5);
-  bb|= (1<<WDCE);
-  ww=bb;
+    byte bb;
+    int ww;
+    if (ii > 9 ) ii = 9;
+    bb = ii & 7;
+    if (ii > 7) bb |= (1 << 5);
+    bb |= (1 << WDCE);
+    ww = bb;
 
-  MCUSR &= ~(1<<WDRF);
-  // start timed sequence
-  WDTCR |= (1<<WDCE) | (1<<WDE);
-  // set new watchdog timeout value
-  WDTCR = bb;
-  WDTCR |= _BV(WDIE);
+    MCUSR &= ~(1 << WDRF);
+    // start timed sequence
+    WDTCR |= (1 << WDCE) | (1 << WDE);
+    // set new watchdog timeout value
+    WDTCR = bb;
+    WDTCR |= _BV(WDIE);
 }
-  
+
 
